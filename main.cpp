a21d518db17a0d23869d8c4ae1a6ded855a7b392
cThomp <cody5887@yahoo.com>
2020-02-15 23:44:47 -0800

    Implemented Lexer.h into main.
    Deleted unnecessary files. ( functions.cpp & Header.h )

--- main.cpp
+++ main.cpp
@@ -6,42 +6,14 @@
 //  Copyright © 2020 Cody Thompson. All rights reserved.
 //
 
-#include "Header.h"
+#include "Lexer.h"
 
 int main( int argc, const char * argv[] ) {
     std::string srcFile = "/Users/codythompson/Documents/Code/MyCompiler/LexicalAnalysis/LexicalAnalysis/myfile.txt";              // This will eventually be passed into the program as an arguement
-    std::queue<std::string> readQu;
-
-    if( !queueInit( srcFile, readQu ) )
-        std::cerr << "The file specified does not exist in this directory.\n";
-
-    std::queue<std::string> tokenQu;
-    initTokenQueue( readQu, tokenQu );
-
-    std::cout << "The queue size is: " << tokenQu.size() << std::endl;
-    while( !tokenQu.empty() ) {
-        std::cout << tokenQu.front() << std::endl;
-        tokenQu.pop();
-    }
+    Lexer lex(srcFile);
+    
+    lex.tokenQueue();
+    lex.output();
     
     return 0;
 }
-
-
-/*
- Regular expressions
-    Indentifiers    =   ([a-zA-Z])([a-zA-Z0-9$*]*)                              Alphabetic symbol followed by digit, alhpabetic character, or $
-    Keywords        =   [a-z]{2,8}
-    Separators      =   [\'|\(|\)|\{|\}|\[|\]|\,|\.|\:|\;| ]{1}                 ~ Not sure if this is what we want
-    Block Comments  =   !([a-zA-Z0-9 ]*)!                                       "!" symbol followed by 0 >= alaphabetic characters, digits or spaces ending with "!" symbol
- */
-
-
-
-
-
-
-
-
-
-
